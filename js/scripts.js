fd03adf68659f99e3f1ac61bf7e052c810cf057d
kusnetsov-star <kusnetsov888@gmail.com>
2025-11-01 10:59:14 +0500

    Урок4, закончили разработку сайта

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const schemeSvg = document.querySelector(".scheme-svg");
-const totalPriceTag = document.querySelector(".price-total")
+const totalPriceTag = document.querySelector(".price-total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
 let cost = 500;
 let totalPrice = 0;
 
@@ -16,3 +18,7 @@ schemeSvg.addEventListener("click", (event) => {
     totalPriceTag.textContent = totalPrice;
   }
 });
+menuButton.addEventListener('click', () => {
+  console.log("Кликнули");
+  menu.classList.toggle('is-open');
+})
